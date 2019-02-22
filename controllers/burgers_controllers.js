fb09aa23a23b52d2211d6f5f22e8170eb54fb0b0
Olivia Fontanese <olfo4481@colorado.edu>
2019-02-22 16:53:00 -0500

    file structure complete

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -8,3 +8,10 @@
 
 
 // Create the router for the app, and export the router at the end of your file.
+
+var express = require("express");
+var router = express.Router();
+
+router.get("/",function(req,res){
+    res.render("index");
+});
